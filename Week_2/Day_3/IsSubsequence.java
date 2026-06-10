39f709cf3d50778b54e26f47b23e21cdad37c675
DhruvSoni <dhruvsoni0721@gmail.com>
2026-06-10 23:33:46 +0530

    Week 2 Day 3 Is Subsequence

---
+++ Week_2/Day_3/IsSubsequence.java
@@ -0,0 +1,28 @@
+// package Week_2.Day_3;
+
+public class IsSubsequence {
+     public static boolean isSubsequence(String s, String t) {
+        int i=0,j=0;
+
+        while(i<s.length() && j<t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return (i==s.length());
+    }
+    public static void main(String[] args)
+    {
+        String s = "abc";
+        String t = "ahbgdc";
+
+        System.out.println(isSubsequence(s, t));
+    }
+}
