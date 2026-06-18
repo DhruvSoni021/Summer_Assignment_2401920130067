1f586c27170dc0150ba0038f18ec50307fcca8d0
DhruvSoni <dhruvsoni0721@gmail.com>
2026-06-19 00:21:32 +0530

    Week 3 Day 4 Evaluate Reverse Polish Notation

---
+++ Week_3/Day_4/ReversePolish.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ReversePolish
+{
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+
+        for(String s : tokens) 
+        {
+            if(s.equals("+")) 
+            {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a + b);
+            }
+            else if(s.equals("-")) 
+            {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a - b);
+            }
+            else if(s.equals("*")) 
+            {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a * b);
+            }
+            else if(s.equals("/")) 
+            {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a / b);
+            }
+            else 
+            {
+                st.push(Integer.parseInt(s));
+            }
+        }
+
+        return st.pop();
+    }
+    
+}
