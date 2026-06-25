3fb88388ab5899a566f8d8e553da27a956dc97f6
DhruvSoni <dhruvsoni0721@gmail.com>
2026-06-25 15:20:11 +0530

    Week 4 Day 4 Binary Tree Maximum Path Sum

---
+++ Week_4/Day_4/MaxPathSumBinaryTree.java
@@ -0,0 +1,18 @@
+import javax.swing.tree.TreeNode;
+
+public class MaxPathSumBinaryTree{
+     public int maxPathSum(TreeNode root) {
+        int[] maxValue = new int[1];
+        maxValue[0] = Integer.MIN_VALUE;
+        maxPathDown(root,maxValue);
+        return maxValue[0];
+    }
+    public int maxPathDown(TreeNode root, int[] maxValue)
+    {
+        if(root == null) return 0;
+        int left = Math.max(0,maxPathDown(root.left,maxValue));
+        int right = Math.max(0,maxPathDown(root.right,maxValue));
+        maxValue[0] = Math.max(maxValue[0], left + right + root.val);
+        return root.val + Math.max(left , right) ;
+    }
+}
