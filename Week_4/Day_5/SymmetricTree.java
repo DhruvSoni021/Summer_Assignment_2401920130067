d3d3f49a08ccc7bb9d846c85534a9f46667dc14e
DhruvSoni <dhruvsoni0721@gmail.com>
2026-06-27 02:04:33 +0530

    Week 4 Day 5 Symmetric Tree

---
+++ Week_4/Day_5/SymmetricTree.java
@@ -0,0 +1,19 @@
+// package Week_4.Day_5;
+
+import javax.swing.tree.TreeNode;
+
+public class SymmetricTree {
+     public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetrichelp(root.left, root.right);
+    }
+    public boolean isSymmetrichelp(TreeNode left, TreeNode right) {
+        if(left == null || right == null) return left == right;
+
+        if(left.val != right.val) return false;
+
+        return (isSymmetrichelp(left.left,right.right)) 
+                &&
+                (isSymmetrichelp(left.right, right.left));
+    
+    }
+}
